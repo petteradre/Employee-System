76184b9c835f952adf645bea11baa446a234c34c
Pedro Adan Bustos de Haro <pedro_adan.deharo31@hotmail.com>
2020-08-05 22:28:57 -0500

    Employee System First Commit

---
+++ EmployeeSystem/Models/City.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeSystem.Data.Models
+{
+    public class City
+    {
+        public int CityId { get; set; }
+        public string CityName { get; set; }
+    }
+}
---
+++ EmployeeSystem/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace EmployeeSystem.Data.Models
+{
+    public class Employee
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Department { get; set; }
+        public string Gender { get; set; }
+    }
+}
